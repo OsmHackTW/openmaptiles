4141b845f78d594e95bc71f5c6168a0266a7d4a2
typebrook <typebrook@gmail.com>
2019-06-21 01:20:49 +0000

    Log generated changed tiles
    Put them into data/changed-tiles before each generate action for changed tiles

--- update/get-tiles.sh
+++ update/get-tiles.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+mkdir -p /export/changed-tiles
+TILE_LOG=/export/changed-tiles/tiles-$(date +%m%d%H%M)
 FILE_LIST=/export/files
 
-find /export -name *.tiles | tee FILE_LIST | \
-xargs cat | sort -u > /export/tiles.txt && \
-cat FILE_LIST | xargs rm FILE_LIST
+find /export -name *.tiles | tee $FILE_LIST | \
+xargs cat | sort -u | tee /export/tiles.txt > $TILE_LOG && \
+cat $FILE_LIST | xargs rm $FILE_LIST
